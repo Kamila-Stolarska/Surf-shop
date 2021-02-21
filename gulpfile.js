a56aa6dd6df7882310142da2c9988a966c9880c2
Kamila Stolarska <kamila0212@gmail.com>
2021-02-21 13:32:14 +0000

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sourcemaps = require("gulp-sourcemaps");
 var browserSync = require("browser-sync").create();
 
 gulp.task("watch", function (cb) {
-  gulp.watch("style/*.scss", gulp.series("sass"));
+  gulp.watch("style/**/*.scss", gulp.series("sass"));
   cb();
 });
 
@@ -12,7 +12,7 @@ gulp.task("serve", function (cb) {
   browserSync.init({
     server: "./"
   });
-  gulp.watch("style/*.scss", gulp.series("sass"));
+  gulp.watch("style/**/*.scss", gulp.series("sass"));
   gulp.watch("*.html").on("change", browserSync.reload);
   gulp.watch("*.js").on("change", browserSync.reload);
   cb();
@@ -21,9 +21,9 @@ gulp.task("serve", function (cb) {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function () {
   return gulp
-    .src("style/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .src("style/**/*.scss")
     .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("css"))
     .pipe(browserSync.stream());
