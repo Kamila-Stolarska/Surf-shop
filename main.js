f8323011458f4425a4cec3d4360faf87cbf20971
Kamila Stolarska <kamila0212@gmail.com>
2021-02-24 23:22:22 +0000

    menu created

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// slider image
 const slideList = [{
     img: "./img/slider1.jpg",
 },
@@ -18,9 +19,35 @@ const changeSlide = () => {
     if(active === slideList.length) {
         active = 0;
     }
-    //console.log("zmaiana")
     image.src = slideList[active].img;
     image.className = `img-slider-${active+1}`;
 }
 
 setInterval(changeSlide, time)
+
+// hidden-menu
+
+let hamburger = document.querySelector(".fas.fa-bars");
+const hiddenMenu = document.querySelector("header .hidden-menu");
+
+
+hamburger.addEventListener('click', function() {
+// hiddenMenu.style.display = "block";
+ hiddenMenu.classList.toggle("active");
+// if ( x == "1") {
+//     hiddenMenu.style.display = "none";
+// } else {
+//     x--;
+//     hiddenMenu.style.display = "block"
+// }
+
+});
+
+// bars.addEventListener('click', function () {
+//     navList.classList.toggle('active');
+// });
+
+// function showMenu() {
+   
+// } 
+// hamburger.addEventListener('click', showMenu)
